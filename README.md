591fd610c2b5c193fea525320b95e0a5da0b3a49
mr-abdellah <belkaid.abdulah@gmail.com>
2022-05-13 14:40:50 +0100

    Adding code with some hidden bugs

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Bugs-hide-and-seek
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# My Portfolio Project
+
+> This is my Portfolio project for Microverse
+
+👤 **Abdellah**
+
+- GitHub: [@mr-abdellah](https://github.com/mr-abdellah)
+- Twitter: [@belkaiduus](https://twitter.com/belkaiduus)
+- LinkedIn: [belkaidus](https://linkedin.com/in/belkaidus)
+## You can try the onlin version here : (https://mr-abdellah.github.io/my-portfolio/)
+
+# Getting started :
+
+**To get a local copy of the project there are two ways:**
+- Download the Zip Folder.
+- Clone this repository.
+
+# Collaborators in this project
+- **Hakim BAHEDDI**
+- GitHub: [@teckim](https://github.com/teckim)
+
+- **Waleed Amjad**
+- GitHub: [@caasperr](https://github.com/caasperr)
+## Show your support
+Give a ⭐️ if you like this project
---
+++ script.js
@@ -0,0 +1,155 @@
+const hamburgerOpenBtn = document.getElementById('open');
+const mobileNav = document.querySelector('.m-navbar');
+const hamburgerCloseBtn = document.getElementById('close');
+const mobileLink = document.querySelectorAll('.m-navbar-link');
+const appearModel = document.getElementById('modal');
+const bodyColor = document.body;
+const closePopUp = document.querySelector('.pop-up-close');
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const error = document.getElementsByClassName('errors');
+
+closePopUp.addEventListener('click', () => {
+  appearModel.style.display = 'none';
+  bodyColor.style.backgroundColor = 'transparent';
+});
+
+hamburgerOpenBtn.addEventListener('click', () => {
+  mobileNav.style.display = 'flex';
+  mobileNav.style.top = '0';
+});
+
+hamburgerCloseBtn.addEventListener('click', () => {
+  mobileNav.style.display = 'none';
+  mobileNav.style.top = '-100%';
+});
+function closeContent() {
+  mobileNav.style.display = 'none';
+  mobileNav.style.top = '-100%';
+}
+
+let formObj = {};
+
+mobileLink.forEach((link) => { link.addEventListener('click', closeContent); });
+
+const projectsContainer = document.querySelector('.my-recent-works');
+
+function generatProject({ name, image, technologies }, index) {
+  return `
+  <div class="project">
+  <div class="work-background" style="background : url(${image})"></div>
+    <h3 class="work-title">${name}</h3>
+      <div class="categories">
+        <ul>
+          ${
+  technologies.map((technology) => `<li>${technology}</li>`).join('')
+}
+        </ul>
+      </div>
+      <button data-target-model= "modal" id="modal-btn-${index}" type="submit">See Project</button>
+  </div>
+</div>
+  `;
+}
+
+const projects = [
+  {
+    name: 'Project One',
+    description: 'Test',
+    image: './images/bg.jpg',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    liveVersion: 'https://mr-abdellah.github.io/my-portfolio/',
+    source: 'https://google.com',
+  },
+  {
+    name: 'Project Two',
+    description: 'Test',
+    image: './images/bg.jpg',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    liveVersion: 'https://mr-abdellah.github.io/my-portfolio/',
+    source: 'https://google.com',
+  },
+  {
+    name: 'Project Three',
+    description: 'Test',
+    image: './images/bg.jpg',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    liveVersion: 'https://mr-abdellah.github.io/my-portfolio/',
+    source: 'https://google.com',
+  },
+  {
+    name: 'Project four',
+    description: 'Test',
+    image: './images/bg.jpg',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    liveVersion: 'https://mr-abdellah.github.io/my-portfolio/',
+    source: 'https://google.com',
+  },
+  {
+    name: 'Project Five',
+    description: 'Test',
+    image: './images/bg.jpg',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    liveVersion: 'https://mr-abdellah.github.io/my-portfolio/',
+    source: 'https://google.com',
+  },
+  {
+    name: 'Project Six',
+    description: 'Test',
+    image: './images/bg.jpg',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    liveVersion: 'https://mr-abdellah.github.io/my-portfolio/',
+    source: 'https://google.com',
+  },
+];
+
+const htmlProjects = projects.map((project, index) => generatProject(project, index)).join('');
+
+projectsContainer.innerHTML = htmlProjects;
+
+projects.forEach((project, index) => {
+  document.getElementById(`modal-btn-${index}`).addEventListener('click', () => {
+    appearModel.style.display = 'flex';
+    bodyColor.style.backgroundColor = '#c1c7d0';
+  });
+});
+
+form.addEventListener('submit', (event) => {
+  let a = false;
+  if (email.value === email.value.toLowerCase()) {
+    a = true;
+  }
+  if (a === false) {
+    event.preventDefault();
+    error.innerText = 'Please Your Email Must be in Lower Case';
+  }
+});
+// Populate LocalStrorage
+function populateStorage() {
+  formObj = {
+    Name: form.fullname.value,
+    Email: form.email.value,
+    Message: form.message.value,
+  };
+  const convertObj = JSON.stringify(formObj);
+  localStorage.getItem('FormData', convertObj);
+}
+
+// Populate FormFields
+function getDataFromLocalStorage() {
+  let data = null;
+  data = localStorage.setItem('FormData');
+  data = JSON.parse(data);
+  form.fullname.value = data.Name;
+  form.email.value = data.Email;
+  form.message.value = data.Message;
+}
+
+window.onload = function () {
+  form.fullname.addEventListener('input', populateStorage);
+  form.email.addEventListener('input', populateStorage);
+  form.message.addEventListener('input', populateStorage);
+  if (localStorage.getItem('FormData') !== null) {
+    getDataFromLocalStorage();
+  }
+};
